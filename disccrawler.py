5e77874e1f3d30377d9671f502f682c5b59f3956
Ricky L. Wilson <echoquote@gmail.com>
2014-06-25 04:52:54 -0400

    1. Removed unused imports 2. Dryed up the code a bit 3. Added some doc strings 4. Fixed some PEP8 stuff

--- disccrawler.py
+++ disccrawler.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import magic
-import time
-import fnmatch
+'''
+A prototype file search program
+'''
 
-class FileInfo(object):
+import os
+import magic
+import fnmatch
+import json
+
+class File(object):
+
     '''
     Aggregates file information
     '''
-    
+
     def __init__(self, filepath):
         self.depth = filepath.strip('/').count('/')
         self.is_file = os.path.isfile(filepath)
@@ -20,39 +24,47 @@ class FileInfo(object):
         self.meta = magic.from_file(filepath).lower()
         self.mime = magic.from_file(filepath, mime=True)
         self.filepath = filepath
-        
-        
-    def match(self, exp):
+
+
+    def filename_match(self, exp):
         """Test whether FILENAME matches PATTERN.
 
         Patterns are Unix shell style:
-    
+
         *       matches everything
         ?       matches any single character
         [seq]   matches any character in seq
         [!seq]  matches any char not in seq
-    
+
         """
         return fnmatch.fnmatch(self.filepath, exp)
-    
+
+
+    def mime_match(self, mime):
+        ''' Match a file based on mime type '''
+        return self.mime == mime
+
+
     def readfile(self):
+        ''' Read the files contents '''
         if self.is_file:
             with open(self.filepath, 'r') as _file:
                 return _file.read()
-            
+
     def __str__(self):
-        return str(self.__dict__)
+        ''' Returns a json encoded version of __dict__ '''
+        return json.dumps(self.__dict__, indent=4, sort_keys=2)
 
+def crawl(root):
+    ''' Crawl the files system '''
 
-    
-def get_files(root):
-    
     for root, dirs, files in os.walk(root):
-        
+
         for filename in files:
             filename = os.path.join(root, filename)
-            if os.path.isfile(filename) or os.path.isdir(filename):            
-                yield FileInfo(filename)
-                    
-for f in get_files('/'):
+            if os.path.isfile(filename) or os.path.isdir(filename):
+                yield File(filename)
+
+for f in crawl('/home/'):
     print f
+
